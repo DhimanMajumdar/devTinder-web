4e3bea7e743322012dbc18889ea8ddc427dbc7de
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2024-10-15 00:23:06 +0530

    redirect to login if not auth

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
-import NavBar from './NavBar'
-import { Outlet } from 'react-router-dom'
-import Footer from './Footer'
-
+import React, { useEffect } from "react";
+import NavBar from "./NavBar";
+import { Outlet, useNavigate } from "react-router-dom";
+import Footer from "./Footer";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import axios from "axios";
 
 const Body = () => {
-  return (<>
-    <NavBar/>
-    <Outlet/>
-    <Footer/>
-  </>)
-}
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+  const fetchUser = async () => {
+    try {
+      if(userData) return;
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if(err.status===401) navigate("/login");
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    fetchUser();
+  }, []);
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
-export default Body
+export default Body;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   const user=useSelector((store)=>store.user);
-  //console.log(user);
+  console.log(user);
   return (
     <div className="navbar bg-[#d5883f] text-white">
       <div className="flex-1">
-        <a className="btn hover:bg-[#D2B48C] hover:text-[#4B3621] transition duration-300 ease-in-out text-xl">🧑‍💻 devTinder</a>
+        <Link to="/" className="btn hover:bg-[#D2B48C] hover:text-[#4B3621] transition duration-300 ease-in-out text-xl">🧑‍💻 devTinder</Link>
       </div>
      
       {user && (<div className="flex-none gap-2">
@@ -26,10 +27,10 @@ const NavBar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
             <li>
-              <a className="justify-between">
+              <Link className="justify-between">
                 Profile
                 <span className="badge">New</span>
-              </a>
+              </Link>
             </li>
             <li><a>Settings</a></li>
             <li><a>Logout</a></li>
